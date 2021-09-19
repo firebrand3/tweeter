012c15ac54d80a496f1f368b675a7f9cfaf9f6d9
Asim <asim.awan@outlook.com>
2021-09-19 16:26:44 +0000

    Created js file using JQuery elements to display character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  console.log("counter loaded");
+
+  $("textarea").keydown(function () {
+    let count = $(this).val().length;
+
+    if (count <= 140) {
+      $(this)
+        .closest(".new-tweet")
+        .find(".counter")
+        .removeClass("negative-count") /* removing a new class */ 
+        .text(140 - count);
+    } else {
+      $(this)
+        .closest(".new-tweet")
+        .find(".counter")
+        .addClass("negative-count") /* adding a new class that can be used in new-tweet.css to handle color change of counter*/
+        .text(140 - count);
+    }
+  });
+});
