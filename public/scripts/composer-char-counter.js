d51fdd1ab17f0761e01e971cb74763d04cc5d053
Asim <asim.awan@outlook.com>
2021-09-28 22:48:07 +0000

    Refactored to display appropriate message after tweet text validation

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -10,12 +10,18 @@ $(document).ready(function () {
         .find(".counter")
         .removeClass("negative-count") /* removing a new class */ 
         .text(140 - count);
+        // $(".error-message").hide();
+        // $(".error-message").empty();
+        $(".error-message").slideUp("slow");
     } else {
       $(this)
         .closest(".new-tweet")
         .find(".counter")
         .addClass("negative-count") /* adding a new class that can be used in new-tweet.css to handle color change of counter*/
         .text(140 - count);
+        $(".error-message").empty();
+        $(".error-message").append('<i class="fas fa-exclamation-triangle"></i> << Your tweet is too long, Plz respect arbitrary limit of 140 chars >> <i class="fas fa-exclamation-triangle"></i>')
+        $(".error-message").slideDown("slow");
     }
   });
 });
