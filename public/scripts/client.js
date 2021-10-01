3a943bb2f44aeef8cb478bd8c5974cb6671a4a5b
Asim <asim.awan@outlook.com>
2021-10-01 23:29:07 +0000

    Final clean up and refactoring

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,8 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Create HTML elements to display tweets
 const createTweetElement = function(obj) {
-const $tweet = `  
+  const $tweet = `  
   <article>
         <header>
           <div class="profile"><img src=${obj.user.avatars}></div>
@@ -23,92 +24,92 @@ const $tweet = `
         </footer>
   </article>
     `;
-  console.log($tweet)
+  console.log($tweet);
   return $tweet;
-}
+};
 
-
+// Loops through all the tweets and creates HTML code for each by calling createTweetElement function
 const renderTweets = function(tweets) {
   // $('#tweet-container').replaceWith('<section id ="tweet-container"></section>')
   $('#tweet-container').empty();
   for (obj of tweets) {
-    $('#tweet-container').prepend(createTweetElement(obj))
+    $('#tweet-container').prepend(createTweetElement(obj));
   }
-}
+};
 
-const escape = function (str) {
+// Function to prevent "Cross-Site Scripting" (XSS)
+const escape = function(str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
+// Create HTML elements to display errors
 const errorMessage = tweetLength => {
   if (tweetLength === 'long') {
     $(".error-message").hide();
     $(".error-message").empty();
-    $(".error-message").append('<i class="fas fa-exclamation-triangle"></i> << Your tweet is too long, Plz respect arbitrary limit of 140 chars >> <i class="fas fa-exclamation-triangle"></i>')
+    $(".error-message").append('<i class="fas fa-exclamation-triangle"></i> << Your tweet is too long, Plz respect arbitrary limit of 140 chars >> <i class="fas fa-exclamation-triangle"></i>');
     $(".error-message").slideDown("slow");
   } else if (tweetLength === 'empty') {
     $(".error-message").hide();
     $(".error-message").empty();
-    $(".error-message").append('<i class="fas fa-exclamation-triangle"></i> << Your tweet is empty >> <i class="fas fa-exclamation-triangle"></i>')
+    $(".error-message").append('<i class="fas fa-exclamation-triangle"></i> << Your tweet is empty >> <i class="fas fa-exclamation-triangle"></i>');
     $(".error-message").slideDown("slow");
   } else {
     $(".error-message").hide();
     $(".error-message").empty();
   }
-}
+};
 
-
+// Ensure page is ready then run any script
 $(document).ready(() => {
 
-  // const loadTweets = function() {
-  //   $.get("/tweets", function(data) {
-  //   console.log(data)
-  //   renderTweets(data);
-  //   });
-  // }
-    const loadTweets = function() {
-      $.ajax({
-        url: "/tweets",
-        type: 'GET',
-        dataType: 'JSON',
-        success: (res) => {
-          // console.log(res)
-          renderTweets(res);
-        },
+  // Function to manage textarea for longer tweets
+  $("textarea").on("input", function() {
+    this.style.height = this.scrollHeight + "px";
+  });
+
+  // Function that uses GET method to load tweets using renderTweets and createTweetElement function
+  const loadTweets = function() {
+    $.ajax({
+      url: "/tweets",
+      type: 'GET',
+      dataType: 'JSON',
+      success: (res) => {
+        // console.log(res)
+        renderTweets(res);
+      },
       error: (error) => {
         alert("Error on tweet!", error);
-      }  
-      });
-    }
+      }
+    });
+  };
+  // Defines whats happens after clicking the TWEET button
   $("form").submit(function(event) {
     event.preventDefault();
-    // alert( $( this ).serialize() );
-    const formData = $(this).serialize();
-    // alert( "Handler for .submit() called." );
-    const formDataLength = formData.length
+    // Serialize and decode tweet text espacially blank spaces
+    const formData = decodeURIComponent($(this).serialize());
+
+    const formDataLength = formData.length;
 
     if (formDataLength === 5) {
-      // alert("tweet is empty")
-      errorMessage("empty")
-    }else if (formDataLength > 145) {
-      errorMessage("long")
-      // alert("exceeded max charcter limit of 140")
+      errorMessage("empty");
+    } else if (formDataLength > 145) {
+      errorMessage("long");
     } else {
+      // POST the new tweet after validating tweet length
       errorMessage();
-     // $.post("/tweets", formData)
       $.ajax({
         url: "/tweets",
         type: 'POST',
         data: formData,
       }).then((res) => {
-        // renderTweets(res);
         loadTweets(res);
-        }); 
-      };
-      $('textarea').val("")
+      });
+    }
+    $('textarea').val("");
   });
 
-    loadTweets();
+  loadTweets();
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,27 +1,25 @@
-$(document).ready(function () {
-  // console.log("counter loaded");
+// Function to validate tweet length and display appropriate error messages
+$(document).ready(function() {
 
-  $("textarea").keyup(function () {
+  $("textarea").keyup(function() {
     let count = $(this).val().length;
 
     if (count <= 140) {
       $(this)
         .closest(".new-tweet")
         .find(".counter")
-        .removeClass("negative-count") /* removing a new class */ 
+        .removeClass("negative-count") /* removing a new class */
         .text(140 - count);
-        // $(".error-message").hide();
-        // $(".error-message").empty();
-        $(".error-message").slideUp("slow");
+      $(".error-message").slideUp("slow");
     } else {
       $(this)
         .closest(".new-tweet")
         .find(".counter")
         .addClass("negative-count") /* adding a new class that can be used in new-tweet.css to handle color change of counter*/
         .text(140 - count);
-        $(".error-message").empty();
-        $(".error-message").append('<i class="fas fa-exclamation-triangle"></i> << Your tweet is too long, Plz respect arbitrary limit of 140 chars >> <i class="fas fa-exclamation-triangle"></i>')
-        $(".error-message").slideDown("slow");
+      $(".error-message").empty();
+      $(".error-message").append('<i class="fas fa-exclamation-triangle"></i> << Your tweet is too long, Plz respect arbitrary limit of 140 chars >> <i class="fas fa-exclamation-triangle"></i>');
+      $(".error-message").slideDown("slow");
     }
   });
 });
